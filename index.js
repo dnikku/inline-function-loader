d0f6b3ce051e5bc0ecb7c36b22250a4b4b0ec2cf
dnikku <dnikku@gmail.com>
2016-09-13 15:41:10 +0200

    make cacheable by default

--- index.js
+++ index.js
@@ -3,14 +3,17 @@
 var loaderUtils	= require("loader-utils");
 
 module.exports = function (content) {
-  //console.log('\n' + this.request);
-
   var query = loaderUtils.parseQuery(this.query);
+  var cacheable = (typeof query.cacheable !== 'undefined') ? true : query.cacheable;
+
+  if (cacheable ) {
+    this.cacheable(true);
+  }
   var fn = this.options[query.functionName];
   if (typeof fn === 'function'){
     content = fn.call(this, content);
   } else {
-    console.error("query.functionName not found/defined in main exported config. query: " + this.query);
+    console.error("query.functionName not found/defined in main exported config. query: " + this.query + " for resource: " + this.resource);
   }
   return content
 }
